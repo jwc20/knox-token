30e4a6ee73a162da3691fba105e23841a4cf5dfb
jwc20 <choi.jae100@gmail.com>
2025-08-02 14:32:22 +0900

    add utils for creating and deleting token (will be used for handling token when creating/log in users)

---
+++ knox_tokens/utils.py
@@ -0,0 +1,35 @@
+import datetime
+from typing import Tuple
+
+from django.contrib.auth import get_user_model
+
+from knox_tokens.models import KnoxToken
+# from utils.exceptions import KnoxTokenDeleteFailed
+
+User = get_user_model()
+
+
+class CreateToken:
+    def __init__(self, user: User):
+        self.user = user
+
+    def create(self) -> Tuple[str, datetime.datetime]:
+        knox_token, token_value = KnoxToken.objects.create(user=self.user)
+        return token_value, knox_token.expiry
+
+
+class DeleteToken:
+    def __init__(self, user: User, knox_token: KnoxToken | None = None):
+        self.user = user
+        self.knox_token = knox_token
+
+    def delete(self):
+        # TODO: auth_token should be the knox_token
+        deleted_info = self.user.authtoken_set.filter(
+            digest=self.auth_token.digest
+        ).delete()
+        if deleted_info[0] != 1:
+            raise KnoxTokenDeleteFailed()
+
+    def delete_all(self):
+        self.user.authtoken_set.all().delete()
